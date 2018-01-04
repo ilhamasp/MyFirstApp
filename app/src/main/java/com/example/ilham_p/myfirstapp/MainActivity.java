471e075ae60731f35a0080bd528c864604400ae2
ilhamasp <mail.ilhamasp@gmail.com>
2018-01-04 10:31:27 +0700

    Update codes

--- app/src/main/java/com/example/ilham_p/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/ilham_p/myfirstapp/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.ilham_p.myfirstapp;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,6 +28,15 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 0; i < 3; i++) {
             Log.i(TAG, "onCreate: i=" + i);
         }
+
+        FloatingActionButton fab = findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView tView = findViewById(R.id.myTextView);
+                tView.setText("Welcome to Espresso!");
+            }
+        });
     }
 
     @Override
